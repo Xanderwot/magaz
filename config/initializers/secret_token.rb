c3ceea24ed02f8ee8e71ebae1a8aada0435067f6
Xanderwot <xanderwot@gmail.com>
2012-10-15 13:34:51 +0300

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magaz::Application.config.secret_token = '4ec25601a7fa01f28724e2ddfef54b99161e972051da7ee071aa71c0d1d538e3a4df7c9d2d8477f3c23211c514622709f433a98fc54e4515042531467d1fd81b'
